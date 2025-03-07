2ba65c5d27d1e41f448cbb0c189c4465251a7140
austinii30 <austinisaboy@gmail.com>
2025-03-07 16:05:19 +0800

    finish Q 1 and 2

--- hw1/code.R
+++ hw1/code.R
@@ -1,3 +1,9 @@
+############################################################ 
+# Filename: code.R
+# Purpose : 113-2 Statistical Computation and Simulation
+# Author  : Potumas Liu
+# Date    : 2025/03/06
+############################################################ 
 
 # 1.(a)
 # ================================================================
@@ -66,11 +72,12 @@ rm_1b <- function(n, seed=123, rmSeed=NULL) {
 
 # 2.(a)
 # ================================================================ 
-fibN <- function(n, m, same=F, seed=123, rmSeed=NULL) {
+fibN <- function(n, m, sameInit=F, seed=123, rmSeed=NULL) {
     if (n < m+1) stop("'n' must at least be 'm+1'!")
 
+    # m+1 initial values must be given first
     # endogenous initial value
-    if (same) 
+    if (sameInit) 
         res <- rep(rm_1b(1, seed=seed), m+1)
     else
         res <- rm_1b(m+1, seed=seed)
@@ -78,39 +85,39 @@ fibN <- function(n, m, same=F, seed=123, rmSeed=NULL) {
     # initial value by runif()
     if (!is.null(rmSeed)) {
         set.seed(rmSeed)
-        if (same)
+        if (sameInit)
             res <- rep(runif(1), m+1)
         else
             res <- runif(m+1)
     }
 
-    i = m + 2 
-    while (i <= n) {
+    i <- m + 2 
+    while (i <= (n+m+1)) {
         x <- (res[i-1] + res[i-1-m]) %% 1
         res <- c(res, x)
-        i = i + 1
+        i <- i + 1
     }
 
-    return(res)
+    return(res[-(1:(m+1))])
 }
 
 
 # 2.(b)
 # ================================================================ 
-rm_2b <- function(n){
-    
+rm_2b <- function(n, k, method, rmSeed){
+    if (!method %in% c("sample", "ceiling"))
+        stop("'method' should only be 'sample' or 'ceiling'")
+
+    set.seed(rmSeed)
+
+    if (method == "sample")
+        res <- sample(1:k, n, replace=T)     
+
+    if (method == "ceiling")
+        res <- ceiling(runif(n, min=0, max=k))
+
+    return(res)
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- hw1/main.R
+++ hw1/main.R
@@ -1,4 +1,10 @@
-#
+############################################################ 
+# Filename: main.R
+# Purpose : 113-2 Statistical Computation and Simulation
+# Author  : Potumas Liu
+# Date    : 2025/03/06
+############################################################ 
+
 source("./code.R")
 
 # 1 (a)
@@ -23,17 +29,58 @@ testResult <- lapply(x.2a, FUN = function(x) {
         # uniform
         uni.p <- ks.test(x, "punif")$p
         # independent tests
-        gap.p <- ()
-        per.p <- ()
-        uad.p <- ()
+#        gap.p <- ()
+#        per.p <- ()
+#        uad.p <- ()
         
-        res <- c(uni.p, gap.p, per.p, uad.p)
-        return(res)
+#        res <- c(uni.p, gap.p, per.p, uad.p)
+#        return(res)
     })
+
 
 
 # 2 (b)
 # ================================================================
+n = 100000
+x.s <- x.c <- c()
+
+K <- 5:50
+K <- c(50, 100, 200, 300, 400, 500)
+K <- 300:500
+for (k in K) {
+    x.s <- c(x.s, rm_2b(n, k, "sample", rmSeed=2025))
+    x.c <- c(x.c, rm_2b(n, k, "ceiling", rmSeed=2025))
+}
+
+# each column is a random number sample of size n
+x.s <- matrix(x.s, nrow = n)
+x.c <- matrix(x.c, nrow = n)
+x.all <- cbind(x.s, x.c)
+
+pValues <- c()
+for (i in 1:ncol(x.all)) {
+    p <- chisq.test(table(x.all[, i]))$p.value
+    pValues <- c(pValues, p)
+}
+pValues <- matrix(pValues, ncol=2, 
+                  dimnames = list(k=K, 
+                                  method=c("sample", "ceiling")))
+print(pValues)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
