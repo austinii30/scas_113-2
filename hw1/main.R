61e570762dc1b7b8a1815016bf850b1719f9ef05
austinii30 <austinisaboy@gmail.com>
2025-03-07 19:08:07 +0800

    finish (1) (2)

--- hw1/main.R
+++ hw1/main.R
@@ -5,19 +5,23 @@
 # Date    : 2025/03/06
 ############################################################ 
 
+library(randtoolbox)  # for Gap test
 source("./code.R")
+
 
 # 1 (a)
 # ================================================================
 x.1a <- rm_1a(10000, rmSeed=2025)
-ks.test(x.1a, "punif")
+print(ks.test(x.1a, "punif"))
+
 
 # 1 (b)
 # ================================================================
 x.1b <- rm_1b(10000, rmSeed=2025)
-ks.test(x.1b, "punif")
+print(ks.test(x.1b, "punif"))
 
 
+stop()
 
 # 2 (a)
 # ================================================================
@@ -29,9 +33,9 @@ testResult <- lapply(x.2a, FUN = function(x) {
         # uniform
         uni.p <- ks.test(x, "punif")$p
         # independent tests
-#        gap.p <- ()
-#        per.p <- ()
-#        uad.p <- ()
+#       gap.p <- (x)
+        #per.p <- ()
+        #uad.p <- ()
         
 #        res <- c(uni.p, gap.p, per.p, uad.p)
 #        return(res)
@@ -68,6 +72,25 @@ pValues <- matrix(pValues, ncol=2,
 print(pValues)
 
 
+
+# 3 
+# ================================================================
+stop()
+N <- 1000
+n10 <- 10; n50 <- 50; n100 <- 100
+
+res <- c()
+#for () {
+#    res <- c(res, rnorm((n10+n50+n100)*N))
+
+#}
+
+t10.10  <- rt(10, df = 10)
+t50.10  <- rt(50, df = 10)
+t100.10 <- rt(100, df = 10)
+t10.20  <- rt(10, df = 20)
+t50.20  <- rt(50, df = 20)
+t100.20 <- rt(100, df = 20)
 
 
 
